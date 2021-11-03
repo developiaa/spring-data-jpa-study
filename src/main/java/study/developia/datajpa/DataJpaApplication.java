4657dabd3b2517ec4c87dd0966a1340e9673f66e
developiaa <developiaa@gmail.com>
2021-11-03 21:42:31 +0900

    first commit

---
+++ src/main/java/study/developia/datajpa/DataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.developia.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+}
