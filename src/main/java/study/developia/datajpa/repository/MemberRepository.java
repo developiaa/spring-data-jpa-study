8a7be1fa1783b7c331f80999e274f186bb07b844
developiaa <developiaa@gmail.com>
2021-11-07 20:25:26 +0900

    feat : Add jpa query method

--- src/main/java/study/developia/datajpa/repository/MemberRepository.java
+++ src/main/java/study/developia/datajpa/repository/MemberRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import study.developia.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
