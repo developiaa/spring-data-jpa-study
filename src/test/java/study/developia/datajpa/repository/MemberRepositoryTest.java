d16948ac0a3479f4480a06dce6938d53206ee39d
developiaa <developiaa@gmail.com>
2021-11-07 21:11:19 +0900

    feat : Add collection parameter binding

--- src/test/java/study/developia/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/developia/datajpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import study.developia.datajpa.entity.Member;
 import study.developia.datajpa.entity.Team;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -130,6 +131,20 @@ class MemberRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for (MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    void findByNamed(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
         }
     }
 }
