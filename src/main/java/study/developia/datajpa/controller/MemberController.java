07086c4ad71d0fbb3ce8ce6b9bc4e9d062b6eb81
developiaa <developiaa@gmail.com>
2022-02-19 14:33:48 +0900

    feat : Add paging

--- src/main/java/study/developia/datajpa/controller/MemberController.java
+++ src/main/java/study/developia/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.developia.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.developia.datajpa.dto.MemberDto;
 import study.developia.datajpa.entity.Member;
 import study.developia.datajpa.repository.MemberRepository;
 
@@ -26,8 +30,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
